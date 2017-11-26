6afa7bd84dfd99a41fb47a2341ed8d3f583da6f7
Marc-Andre St-Jean <1255342@csimple.org>
2017-11-26 13:08:27 -0500

    modification env provider errer string to long

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -27,6 +28,6 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        Schema::defaultStringLength(191);
     }
 }
